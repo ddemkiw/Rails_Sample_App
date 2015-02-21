078a1e53d9a9dd60cc7669efe3b7406820a332ef
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-21 22:16:29 +0000

    starting again

--- app/controllers/static_pages_controller.rb
+++
@@ -1,4 +0,0 @@
-class StaticPagesController < ApplicationController
-  def controller
-  end
-end
--- test/controllers/static_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
-  test "should get controller" do
-    get :controller
-    assert_response :success
-  end
-
-end
